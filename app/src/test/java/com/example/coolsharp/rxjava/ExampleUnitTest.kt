5ca130803dc950fd641950d11e1b46064b4b20d0
coolsharp <coolsharp@wemakeprice.com>
2018-08-01 18:13:40 +0900

    학습 1일차

--- app/src/test/java/com/example/coolsharp/rxjava/ExampleUnitTest.kt
+++ app/src/test/java/com/example/coolsharp/rxjava/ExampleUnitTest.kt
@@ -1,8 +1,14 @@
 package com.example.coolsharp.rxjava
 
+import io.reactivex.Observable
+import io.reactivex.Observable.create
+import io.reactivex.ObservableEmitter
+import io.reactivex.ObservableOnSubscribe
+import io.reactivex.schedulers.Schedulers
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.reactivestreams.Subscriber
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +17,19 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun test_just() {
+        Observable.just(1, 2, 3, 4, 5)
+                .subscribe(System.out::println)
+    }
+
+    @Test
+    fun test_create() {
+        Observable.create(ObservableOnSubscribe<Int> { emitter ->
+            emitter.onNext(100)
+            emitter.onNext(200)
+            emitter.onNext(300)
+            emitter.onComplete()
+        }
+        ).subscribe(System.out::println)
     }
 }
